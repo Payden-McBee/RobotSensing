d61e4fdbc55145b1921391dbd24b26d55f9c00a6
Payden McBee <c15payden.mcbee@usafa.edu>
2013-12-04 08:58:27 -0700

    I set up LED2 as an output. The left sensor will be on A4. I will also clear ENC before each sample and conversion sequence so it is not already sampling when I want to check a different sensor. I looked at Cadet Ryan Good's code and found that LED2 was on BIT6, and that an interrupt can be declared in the .c file and now in the header file.

--- main.c
+++ main.c
@@ -3,16 +3,27 @@
 int main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+
+  //enable ADC10 subsystem
   ADC10CTL0 = ADC10SHT_3 + ADC10ON + ADC10IE; // ADC10ON, interrupt enabled
+  ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;                // Select SMCLK
+
+  //set up LEDs as outputs
+  P1DIR |= 0x01;                            // Set P1.0 to output direction
+  P1DIR |= 0x06;							// Set P1.6 to output direction
+
+  ADC10CTL0 &= ~ENC; 							//clearing core, stop the sample and conversion sequence
   ADC10CTL1 = INCH_4;                       // input A4
   ADC10AE0 |= BIT4;                         // PA.1 ADC option select
-  ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;                // Select SMCLK
-  P1DIR |= 0x01;                            // Set P1.0 to output direction
+
+
+
 
   for (;;)
   {
-    ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
+    ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start, do this for each sensor
     __bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
+
     if (ADC10MEM < 0x1FF)
       P1OUT &= ~0x01;                       // Clear P1.0 LED off
     else
